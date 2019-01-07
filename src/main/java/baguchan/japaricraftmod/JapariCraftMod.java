d53919344ae55c9308a3f0ca90bc2de5db3beaeb
pentantan <shutux500@gmail.com>
2019-01-07 15:09:38 +0900

    test

--- src/main/java/baguchan/japaricraftmod/JapariCraftMod.java
+++ src/main/java/baguchan/japaricraftmod/JapariCraftMod.java
@@ -35,12 +35,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
-@Mod(modid = JapariCraftMod.MODID, name = JapariCraftMod.MODNAME, version = JapariCraftMod.VERSION, useMetadata = true, updateJSON = "https://github.com/pentantan/JapariCraftMod/blob/master/src/main/japaricraftmod.json", dependencies = "required:forge@[14.23.4.2705,);after:twilightforest@[3.8.689,);")
+@Mod(modid = JapariCraftMod.MODID, name = JapariCraftMod.MODNAME, version = JapariCraftMod.VERSION, useMetadata = true, updateJSON = "https://github.com/pentantan/JapariCraftMod/blob/master/src/main/japaricraftmod.json", dependencies = "required:forge@[14.23.5.2768,);after:twilightforest@[3.8.689,);")
 public class JapariCraftMod {
 
     public static final String MODID = "japaricraftmod";
     public static final String VERSION = "5.1.0";
-    public static final String MODNAME = "JapariCraftMod";
+    public static final String MODNAME = "JapariCraftMod2";
 
 
     //Modの情報を格納する。 mcmod.infoの上位互換
